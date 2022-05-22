ba367a72f1cf05a3aacadb7b86eac482b3349d49
Davi-cell03 <davi.dias271@gmail.com>
2022-05-22 16:13:46 -0300

    first commit

---
+++ src/calculadoraPackage/Operacao.java
@@ -0,0 +1,26 @@
+package calculadoraPackage;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Davi
+ */
+public class Operacao {
+    public double somar(double num1, double num2) {
+        return num1+num2;
+    }
+    public double subtrair(double num1, double num2){
+        return num1-num2;
+    }
+    public double multiplicar(double num1, double num2){
+        return num1*num2;
+    }
+    public double dividir(double num1, double num2){
+        return num1/num2;
+    }
+
+}
